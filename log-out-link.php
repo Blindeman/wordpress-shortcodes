08efa9d40ed1f33cde58ccfa8e000e3c22f0bf5a
Naomi Blindeman <naomi@blindeman.com>
2020-10-23 14:14:07 -0400

    Update log-out-link.php

--- log-out-link.php
+++ log-out-link.php
@@ -3,6 +3,7 @@
  * Create a link on the front-end to log out of the site and be redirected to the home-page
  * There is no styling, do your own or you can contact me and I might help
  * Naomi Blindeman: naomi@blindeman.com
+ * For more shortcodes see https://github.com/Blindeman/wordpress-shortcodes
 */
 
 add_shortcode( 'taf_logout', function () {
